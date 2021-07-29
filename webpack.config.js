43dc21af0a44fa9452314e0a6eb5c2b39c51ab31
John Faldo <john.faldo@trussle.com>
2021-07-29 05:55:04 +0100

    move to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const babelConfig = require('./babel.config.json');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.tsx",
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, './public/js')
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: { contentBase: path.join(__dirname, "src") },
   module: {
     rules: [
       {
@@ -21,7 +26,21 @@ module.exports = {
       {
         test: /\.(css|scss)$/,
         use: ['style-loader', 'css-loader']
-      }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
 };
